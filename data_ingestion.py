5072f9a62c9cd7b2cc2ea4d06b453c10a1475256
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-10-25 15:57:48 +0100

    add basics to data ingestion

--- data_ingestion.py
+++ data_ingestion.py
@@ -3,6 +3,7 @@ from getpass import getpass
 from dotenv import load_dotenv
 from typing import List
 from pydantic import BaseModel, Field
+from camel.agents import ChatAgent
 from camel.loaders import Firecrawl
 
 load_dotenv()
@@ -17,3 +18,39 @@ firecrawl_response = firecrawl.crawl(url="https://www.camel-ai.org/about")
 print(firecrawl_response["status"])
 
 print(firecrawl_response["data"][0]["markdown"])
+
+print("===========================")
+print("========== Agent ==========")
+print("===========================")
+
+agent = ChatAgent(
+    system_message="You're a helpful assistant",
+    message_window_size=10, # [Optional] the length for chat memory
+    )
+
+response = agent.step(f"based on {firecrawl_response}, tell how what is CAMEL")
+print(response.msgs[0].content)
+
+
+print("===========================")
+print("=== Structured Outputs ====")
+print("===========================")
+
+class ArticleSchema(BaseModel):
+    title: str
+    points: int
+    by: str
+    commentsURL: str
+
+
+class TopArticlesSchema(BaseModel):
+    top: List[ArticleSchema] = Field(
+        ..., max_length=5, description="Top 5 stories"
+    )
+
+
+response = firecrawl.structured_scrape(
+    url='https://news.ycombinator.com', response_format=TopArticlesSchema
+)
+
+print(response)
