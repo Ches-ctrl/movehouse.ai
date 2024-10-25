3485f249d3ed0367abdaf67747e2b53b095fe326
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-10-25 15:54:41 +0100

    add tool calls

--- first_agent.py
+++ first_agent.py
@@ -4,6 +4,7 @@ from camel.models import ModelFactory
 from camel.types import ModelPlatformType, ModelType
 from camel.configs import ChatGPTConfig
 from camel.agents import ChatAgent
+from camel.toolkits import MathToolkit, SearchToolkit
 from dotenv import load_dotenv
 
 # Load environment variables from a .env file
@@ -24,7 +25,7 @@ agent = ChatAgent(
     system_message=sys_msg,
     model=model,
     message_window_size=10, # [Optional] the length for chat memory
-    )
+)
 
 # Pre-defined user message
 # usr_msg = 'I am entering the CAMEL-AI hackathon, what are some good ideas around multi-agent systems?'
@@ -36,4 +37,28 @@ usr_msg = input('Please enter your message for the agent: ')
 response = agent.step(usr_msg)
 
 # Check the response (just for illustrative purpose)
+print(response.msgs[0].content)
+
+print("===========================")
+print("======= Tool Calls ========")
+print("===========================")
+
+# =================
+
+# Initialize the agent with list of tools
+agent = ChatAgent(
+    system_message=sys_msg,
+    tools = [
+        *MathToolkit().get_tools(),
+        *SearchToolkit().get_tools(),
+    ]
+)
+
+# Let agent step the message
+response = agent.step("What is CAMEL AI?")
+
+# Check tool calling
+print(response.info['tool_calls'])
+
+# Get response content
 print(response.msgs[0].content)
